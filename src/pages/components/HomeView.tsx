2b57426caf5a47505b20c123bed9b28339ac315c
Bora Özışık <ozisikbora@gmail.com>
2023-05-07 03:08:18 +0300

    about menu and hero style

--- src/pages/components/HomeView.tsx
+++ src/pages/components/HomeView.tsx
@@ -1,8 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  ClickAwayListener,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useRouter } from "next/router";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Dialog } from "@headlessui/react";
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
+import AutoStoriesOutlinedIcon from "@mui/icons-material/AutoStoriesOutlined";
+import Diversity1OutlinedIcon from "@mui/icons-material/Diversity1Outlined";
+import RouteOutlinedIcon from "@mui/icons-material/RouteOutlined";
+import ConnectWithoutContactOutlinedIcon from "@mui/icons-material/ConnectWithoutContactOutlined";
+import PlayCircleOutlineOutlinedIcon from "@mui/icons-material/PlayCircleOutlineOutlined";
 
 type Props = {};
 
@@ -10,6 +29,35 @@ const HomeView = (props: Props) => {
   const router = useRouter();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [top, setTop] = useState(true);
+  const [isShowing, setIsShowing] = useState(false);
+  //When I'm not designing,I'm also passionate about
+
+  const about = [
+    {
+      name: "Story",
+      description: "Learn about my way and see my thoughts",
+      onClick: () => console.log("xdxdxd"),
+      icon: AutoStoriesOutlinedIcon,
+    },
+    {
+      name: "Journey",
+      description: "Explore my path",
+      onClick: () => console.log("xdxdxd"),
+      icon: Diversity1OutlinedIcon,
+    },
+    {
+      name: "Passions",
+      description: "Discover my passions",
+      onClick: () => console.log("xdxdxd"),
+      icon: RouteOutlinedIcon,
+    },
+    {
+      name: "Contact",
+      description: "Connect me anytime",
+      onClick: () => console.log("xdxdxd"),
+      icon: ConnectWithoutContactOutlinedIcon,
+    },
+  ];
 
   useEffect(() => {
     const scrollHandler = () => {
@@ -35,7 +83,7 @@ const HomeView = (props: Props) => {
             <button onClick={() => router.push("/")} className="-m-1.5 p-1.5">
               <img
                 className="md:h-9 sm: h-8 w-auto"
-                src="/static/logos/deneme.svg"
+                src="/static/logos/main-logo.svg"
                 alt=""
               />
             </button>
@@ -52,23 +100,87 @@ const HomeView = (props: Props) => {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             <button
-              className="text-sm font-medium leading-6 text-[#0077b6]"
+              className="text-sm font-medium leading-6 text-[#00bbf9] hover:text-[#f72585] "
               style={{ cursor: "pointer" }}
               onClick={() => router.push("/")}
             >
               Home
             </button>
+
+            <ClickAwayListener onClickAway={() => setIsShowing(false)}>
+              <Popover className="relative">
+                <Popover.Button
+                  className="inline-flex items-center gap-x-1 text-sm font-semibold leading-6"
+                  onClick={() => setIsShowing((isShowing) => !isShowing)}
+                >
+                  <button
+                    className="text-sm font-medium leading-6 text-[#00bbf9]"
+                    onClick={() => router.push("/#contact")}
+                  >
+                    About
+                  </button>
+                  {isShowing === true ? (
+                    <ChevronUpIcon
+                      className="h-5 w-5 text-[#00bbf9]"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <ChevronDownIcon
+                      className="h-5 w-5 text-[#00bbf9]"
+                      aria-hidden="true"
+                    />
+                  )}
+                </Popover.Button>
+
+                <Transition
+                  show={isShowing}
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <Popover.Panel className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-3/4 px-4">
+                    <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
+                      <div className="p-4">
+                        {about.map((item) => (
+                          <div
+                            key={item.name}
+                            className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50"
+                          >
+                            <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                              <item.icon
+                                className="h-6 w-6 text-gray-600 group-hover:text-[#f72585]"
+                                aria-hidden="true"
+                              />
+                            </div>
+                            <div>
+                              <button
+                                onClick={item.onClick}
+                                className="font-semibold text-gray-900"
+                              >
+                                {item.name}
+                                <span className="absolute inset-0" />
+                              </button>
+                              <p className="mt-1 text-gray-600">
+                                {item.description}
+                              </p>
+                            </div>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  </Popover.Panel>
+                </Transition>
+              </Popover>
+            </ClickAwayListener>
             <button
-              className="text-sm font-medium leading-6 text-[#0077b6]"
+              className="text-sm font-medium leading-6 text-[#00bbf9] hover:text-[#f72585] "
               onClick={() => router.push("/#contact")}
             >
-              About
-            </button>
-            <button
-              className="text-sm font-medium leading-6 text-[#0077b6]"
-              onClick={() => router.push("/#contact")}
-            >
-              Contact
+              Work
             </button>
           </div>
         </nav>
@@ -84,7 +196,7 @@ const HomeView = (props: Props) => {
               <button className="-m-1.5 p-1.5" onClick={() => router.push("/")}>
                 <img
                   className="h-10 w-auto"
-                  src="/static/logos/deneme.svg"
+                  src="/static/logos/main-logo.svg"
                   alt=""
                   onClick={() => setMobileMenuOpen(false)}
                 />
@@ -126,17 +238,32 @@ const HomeView = (props: Props) => {
       </header>
       {/* // -------------------Header----------------------  */}
       {/* // -------------------Hero----------------------  */}
-      <div className="px-6 py-24 md:px-12 text-gray-800 text-center lg:text-left">
+      <div className="px-6 py-20 md:px-12 text-gray-800 text-center lg:text-left">
         <div className="container mx-auto xl:px-32">
           <div className="grid lg:grid-cols-2 flex items-center">
             <div className="md:mt-12 lg:mt-0 mb-12 lg:mb-0">
               <div className="block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-14 bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg">
-                <h1 className="text-4xl md:text-5xl xl:text-6xl font-bold tracking-tight mb-12">
-                  The best Bora <br />
-                  <span className="text-blue-600">for your business</span>
+                <h1 className="text-4xl md:text-3xl xl:text-4xl font-medium tracking-tight mb-12">
+                  Bora Özışık
+                  <img
+                    className="w-44 lg:ml-0 h-auto -mb-12"
+                    src="/static/handDraw/handdraw-funUnderlineTiny.svg"
+                    alt=""
+                  />
+                  <br />
+                  <Stack direction={"row"} alignItems={"center"}>
+                    <span className="text-[#00bbf9]">
+                      Empowering myself and others to reach new heights.{" "}
+                    </span>
+                    <img
+                      className="h-12 w-auto -mb-28"
+                      src="/static/handDraw/handdrawHeart.svg"
+                      alt=""
+                    />
+                  </Stack>
                 </h1>
                 <a
-                  className="inline-block px-7 py-3 mb-2 md:mb-0 md:mr-2 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  className="inline-block px-7 py-3 mb-2 md:mb-0 md:mr-2 bg-[#00bbf9] text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-[#f72585] hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
                   href="#!"
@@ -145,7 +272,7 @@ const HomeView = (props: Props) => {
                   Get started
                 </a>
                 <a
-                  className="inline-block px-7 py-3 bg-transparent text-blue-600 font-medium text-sm leading-snug uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
+                  className="inline-block px-7 py-3 bg-transparent text-[#00bbf9] font-medium text-sm leading-snug uppercase rounded hover:text-[#f72585] hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
                   data-mdb-ripple="true"
                   data-mdb-ripple-color="light"
                   href="#!"
