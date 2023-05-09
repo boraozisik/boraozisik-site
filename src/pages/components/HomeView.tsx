ccf1aae8204efb0769e66254690cad40d0eeffee
Bora Özışık <ozisikbora@gmail.com>
2023-05-09 23:37:39 +0300

    skills section done

--- src/pages/components/HomeView.tsx
+++ src/pages/components/HomeView.tsx
@@ -286,15 +286,6 @@ const HomeView = (props: Props) => {
                 >
                   Get started
                 </a>
-                <a
-                  className="inline-block px-7 py-3 bg-transparent text-[#00bbf9] font-medium text-sm leading-snug uppercase rounded hover:text-[#f72585] hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="light"
-                  href="#!"
-                  role="button"
-                >
-                  Learn more
-                </a>
               </div>
             </div>
             <div className="md:mb-12 lg:mb-0">
@@ -397,6 +388,390 @@ const HomeView = (props: Props) => {
         </div>
       </section>
       {/* // -------------------Briefly About----------------------  */}
+      {/* // -------------------Skills----------------------  */}
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="flex flex-col mb-6 lg:flex-row md:mb-10">
+          <div className="lg:w-1/2">
+            <h2 className="max-w-md mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none xl:max-w-lg">
+              <span className="relative inline-block px-2">
+                <div className="absolute inset-0 transform -skew-x-12 bg-[#f72585]" />
+                <span className="relative text-white">Skill</span>
+              </span>{" "}
+              and{" "}
+              <span className="relative inline-block">
+                <span className="relative text-gray-800">Passion</span>
+
+                <img
+                  src="../static/handDraw/handdraw-underlinetiny.svg"
+                  alt=""
+                  className="w-32"
+                />
+              </span>
+              working together...
+            </h2>
+          </div>
+          <div className="lg:w-1/2">
+            <p className="text-base text-gray-700 md:text-lg">
+              With my skills, I am confident in my ability to meet and exceed
+              expectations.I am passionate about what I do and I am trying to
+              improve myself day by day. Here you can have a look at my skills.
+            </p>
+          </div>
+        </div>
+        <div className="grid gap-8 row-gap-10 sm:grid-cols-2 lg:grid-cols-4">
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-7 h-7 text-deep-purple-accent-400"
+                strokeLinecap="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+              >
+                <circle cx="11" cy="13" fill="none" r="9" stroke="#00bbf9" />
+                <path
+                  d=" M21.955,18.005c1.089,2.145,1.378,3.816,0.622,4.572C20.92,24.234,14.799,20.799,9,15S-0.234,3.08,1.423,1.423 C2.18,0.666,3.853,0.956,6,2.047"
+                  stroke="#00bbf9"
+                  fill="none"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">Frontend</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Areas I have mastered in frontend
+            </p>
+            <ul className="mb-4 -ml-1 space-y-2">
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Next Js-React
+              </li>
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                TypeScript
+              </li>
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                GraphQL
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-7 h-7 text-deep-purple-accent-400"
+                viewBox="0 0 24 24"
+                strokeLinecap="round"
+              >
+                <path
+                  d="M2,10C2,5.029,6.47,1,12,1c5.379,0,10,4.029,10,9S15.712,23,12,23C8.212,23,2,14.971,2,10Z"
+                  fill="none"
+                  strokeWidth="2"
+                  stroke="#00bbf9"
+                />
+                <path
+                  d="M5,9a6.529,6.529,0,0,1,4.393,1.607C10.705,11.92,10,15,10,15a6.59,6.59,0,0,1-3.436-1.564C5.022,11.894,5,9,5,9Z"
+                  fill="none"
+                  strokeWidth="1.5"
+                  stroke="#00bbf9"
+                />
+                <path
+                  d="M19,9a6.523,6.523,0,0,0-4.392,1.608C13.3,11.92,14,15,14,15a6.59,6.59,0,0,0,3.436-1.564C18.978,11.894,19,9,19,9Z"
+                  fill="none"
+                  strokeWidth="1.5"
+                  stroke="#00bbf9"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">Backend</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Lets check my backend skills
+            </p>
+            <ul className="mb-4 -ml-1 space-y-2">
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Nest Js
+              </li>
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                PostgreSQL
+              </li>
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                GraphQL
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-7 h-7 text-deep-purple-accent-400"
+                strokeLinecap="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M8.291,18.709,4.182,22.818c-.419.419-1.43.086-2.258-.742s-1.161-1.839-.742-2.258l4.11-4.11"
+                  fill="none"
+                  stroke="#00bbf9"
+                />
+                <ellipse
+                  cx="19.078"
+                  cy="4.922"
+                  fill="none"
+                  rx="2.5"
+                  ry="4.95"
+                  stroke="#00bbf9"
+                  transform="translate(2.107 14.932) rotate(-45)"
+                />
+                <path
+                  d="M9.185,9.815,5.3,13.7c-.7.7-.143,2.382,1.238,3.762S9.6,19.4,10.3,18.7l3.885-3.885"
+                  fill="none"
+                  stroke="#00bbf9"
+                />
+                <path
+                  d="M15.578,1.422,9.422,7.578c-.976.976-.2,3.335,1.732,5.268s4.292,2.708,5.268,1.732l6.156-6.156"
+                  fill="none"
+                  stroke="#00bbf9"
+                />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">
+              Programming Languages
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              PL&apos;s ​​I know, starting from the best
+            </p>
+            <ul className="mb-4 -ml-1 space-y-2">
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                TypeScript
+              </li>
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Java
+              </li>
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Python
+              </li>
+            </ul>
+          </div>
+          <div>
+            <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-indigo-50">
+              <svg
+                className="w-7 h-7 text-deep-purple-accent-400"
+                strokeLinecap="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M8,13l3,3,9.379-9.379a2.122,2.122,0,0,0,0-3h0a2.122,2.122,0,0,0-3,0Z"
+                  fill="none"
+                  stroke="#00bbf9"
+                />
+                <polyline
+                  fill="none"
+                  points="10 11 10 6 6 2 3 5 7 9"
+                  stroke="#00bbf9"
+                />
+                <polyline
+                  fill="none"
+                  points="15 12 15 17 19 21 22 18 18 14"
+                  stroke="#00bbf9"
+                />
+                <path d="M8,23a7,7,0,0,1-7-7" fill="none" stroke="#00bbf9" />
+                <path d="M8,19a3,3,0,0,1-3-3" fill="none" stroke="#00bbf9" />
+              </svg>
+            </div>
+            <h6 className="mb-2 font-semibold leading-5">General</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Also I think I&apos;m good at these skills
+            </p>
+            <ul className="mb-4 -ml-1 space-y-2">
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Teamwork
+              </li>
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Algorithms
+              </li>
+              <li className="flex items-start">
+                <span className="mr-1">
+                  <svg
+                    className="w-5 h-5 mt-px text-deep-purple-accent-400"
+                    stroke="#00bbf9"
+                    viewBox="0 0 52 52"
+                  >
+                    <polygon
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      fill="none"
+                      points="29 13 14 29 25 29 23 39 38 23 27 23"
+                    />
+                  </svg>
+                </span>
+                Problem Solving
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* // -------------------Skills----------------------  */}
     </>
   );
 };
