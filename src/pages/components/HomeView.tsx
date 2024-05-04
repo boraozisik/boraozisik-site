6cbe05bd0c00f4987c7ebb16a7bf0f608c958167
Bora <ozisikbora@gmail.com>
2024-05-05 01:32:43 +0300

    languages and footer style fixes

--- src/pages/components/HomeView.tsx
+++ src/pages/components/HomeView.tsx
@@ -42,7 +42,7 @@ const HomeView = (props: Props) => {
               <a
                 onClick={() => router.push("/about")}
                 title=""
-                className="inline-flex items-center px-6 py-4 mt-8 font-medium text-white transition-all duration-200 bg-[#89ABE3]  rounded-full lg:mt-16 hover:bg-red-500 focus:bg-blue-400"
+                className="inline-flex items-center px-6 py-4 mt-8 font-medium text-white transition-all duration-200 bg-[#89ABE3]  rounded-full lg:mt-16 hover:bg-blue-400 focus:bg-blue-400"
                 role="button"
               >
                 About
@@ -64,7 +64,7 @@ const HomeView = (props: Props) => {
               <a
                 onClick={() => router.push("/work")}
                 title=""
-                className="ml-4 inline-flex items-center px-6 py-4 mt-8 font-medium text-black transition-all duration-200 bg-[#EA738D] rounded-full lg:mt-16 hover:bg-yellow-500 focus:bg-yellow-400"
+                className="ml-4 inline-flex items-center px-6 py-4 mt-8 font-medium text-black transition-all duration-200 bg-[#EA738D] rounded-full lg:mt-16 hover:bg-red-400 focus:bg-yellow-400"
                 role="button"
               >
                 My Work
@@ -578,14 +578,14 @@ const HomeView = (props: Props) => {
       {/* // -------------------Timeline----------------------  */}
       {/* // -------------------Languages----------------------  */}
       <a href="#languages" id={"languages"}></a>
-      <section className="text-gray-100 body-font bg-gray-800">
+      <section className="text-gray-100 body-font bg-gray-100">
         <div className="container px-5 py-12 mx-auto">
           <h1 className="text-2xl font-medium lg:text-3xl  text-center title-font text-[#89ABE3] mb-16">
             Languages
           </h1>
 
           <div className="flex items-center lg:w-full mx-auto border-b pb-10 mb-10 border-gray-200 sm:flex-row flex-col">
-            <div className="sm:w-24 sm:h-24 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <div className="sm:w-20 sm:h-20 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
               <img
                 src="/static/flags/tr-circle-flag.png"
                 alt=""
@@ -596,7 +596,7 @@ const HomeView = (props: Props) => {
               <h2 className="text-[#89ABE3] text-lg title-font font-medium mb-2">
                 Turkish
               </h2>
-              <p className="leading-relaxed text-base font-medium">
+              <p className="leading-relaxed text-base text-gray-800 font-medium">
                 Turkish is my native language and also, since I live in Turkey,
                 I speak this language in my daily life.
               </p>
@@ -610,7 +610,7 @@ const HomeView = (props: Props) => {
               <h2 className="text-[#89ABE3] text-lg title-font font-medium mb-2 font-medium">
                 English
               </h2>
-              <p className="leading-relaxed text-base font-medium">
+              <p className="leading-relaxed text-base text-gray-800 font-medium">
                 I have a very good command of the English language and I am very
                 confident in my English abilities.Also I&apos;m still constantly
                 trying to improve myself.
@@ -619,12 +619,12 @@ const HomeView = (props: Props) => {
                 Professional working proficiency
               </a>
             </div>
-            <div className="sm:w-24 sm:h-24 sm:order-none order-first h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <div className="sm:w-20 sm:h-20 sm:order-none order-first h-20 w-20 sm:ml-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
               <img src="/static/flags/en-circle-flag.png" alt="" />
             </div>
           </div>
           <div className="flex items-center lg:w-full mx-auto sm:flex-row flex-col">
-            <div className="sm:w-24 sm:h-24 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
+            <div className="sm:w-20 sm:h-20 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 flex-shrink-0">
               <img
                 src="/static/flags/ger-circle-flag.png"
                 alt=""
@@ -635,7 +635,7 @@ const HomeView = (props: Props) => {
               <h2 className="text-[#89ABE3] text-lg title-font font-medium mb-2 font-medium">
                 German
               </h2>
-              <p className="leading-relaxed text-base font-medium">
+              <p className="leading-relaxed text-base text-gray-800 font-medium">
                 I&apos;m in the process of studying German. It hasn&apos;t been
                 long since I started learning but I&apos;m working on improving
                 my German language skills.
@@ -649,15 +649,15 @@ const HomeView = (props: Props) => {
       </section>
       {/* // -------------------Languages----------------------  */}
       {/* // -------------------Footer----------------------  */}
-      <footer className="relative z-10 bg-gray-100 pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+      <footer className="relative z-10 bg-gray-800 pt-20 pb-10 lg:pt-[120px] lg:pb-20">
         <div className="container mx-auto">
           <div className=" flex flex-wrap">
             <div className="w-full px-4 sm:w-2/3 lg:w-3/12">
               <div className="mb-10 w-full">
-                <p className="text-body-color mb-7 text-base">
+                <p className="text-gray-100 mb-7 text-base text-gray-100">
                   Designing the Extraordinary, Every Step of the Way.
                 </p>
-                <p className="text-dark flex items-center text-sm font-medium hover:text-[#89ABE3]">
+                <p className="text-gray-100 flex items-center text-sm font-medium hover:text-[#89ABE3]">
                   <span className="text-primary mr-3">
                     <svg
                       width="15"
@@ -670,19 +670,21 @@ const HomeView = (props: Props) => {
                       <path d="M0.802807 6.05619C0.869358 7.52032 2.16711 8.11928 2.83263 8.11928H5.16193C5.19521 8.11928 5.19521 8.11928 5.19521 8.11928C6.19348 8.05273 7.19175 7.38722 7.19175 6.05619V5.25757C8.28985 5.25757 10.8188 5.25757 11.9169 5.25757V6.05619C11.9169 7.38722 12.9152 8.05273 13.9135 8.11928H13.9467H16.2428C16.9083 8.11928 18.206 7.52032 18.2726 6.05619C18.2726 5.95636 18.2726 5.59033 18.2726 5.25757C18.2726 4.99136 18.2726 4.75843 18.2726 4.72516C18.2726 4.69188 18.2726 4.6586 18.2726 4.6586C18.1727 3.72688 17.84 2.96154 17.2743 2.36258L17.241 2.3293C16.4091 1.56396 15.4109 1.13138 14.6455 0.865169C12.416 0 9.62088 0 9.48778 0C7.52451 0.0332757 6.26003 0.199654 4.36331 0.865169C3.63125 1.0981 2.63297 1.53068 1.80108 2.29603L1.7678 2.3293C1.20212 2.92827 0.869359 3.69361 0.769531 4.62533C0.769531 4.6586 0.769531 4.69188 0.769531 4.69188C0.769531 4.75843 0.769531 4.95809 0.769531 5.22429C0.802807 5.52377 0.802807 5.92308 0.802807 6.05619ZM2.5997 3.12792C3.26521 2.52896 4.09711 2.16292 4.7959 1.89672C6.52624 1.26448 7.65761 1.13138 9.55433 1.0981C9.68743 1.0981 12.2829 1.13138 14.2795 1.89672C14.9783 2.16292 15.8102 2.49568 16.4757 3.12792C16.8417 3.52723 17.0746 4.05964 17.1412 4.69188C17.1412 4.79171 17.1412 4.95809 17.1412 5.22429C17.1412 5.55705 17.1412 5.92308 17.1412 6.02291C17.1079 6.78825 16.3759 6.95463 16.276 6.95463H13.98C13.6472 6.92135 13.1148 6.78825 13.1148 6.05619V4.69188C13.1148 4.42567 12.9485 4.22602 12.7155 4.12619C12.5159 4.05964 6.69262 4.05964 6.49296 4.12619C6.26003 4.19274 6.09365 4.42567 6.09365 4.69188V6.05619C6.09365 6.78825 5.56124 6.92135 5.22848 6.95463H2.93246C2.83263 6.95463 2.10056 6.78825 2.06729 6.02291C2.06729 5.92308 2.06729 5.55705 2.06729 5.22429C2.06729 4.95809 2.06729 4.82498 2.06729 4.72516C2.00073 4.05964 2.23366 3.52723 2.5997 3.12792Z" />
                     </svg>
                   </span>
-                  <span className="font-medium">+90 554 595 99 05</span>
+                  <span className="font-medium text-gray-100">
+                    +90 554 595 99 05
+                  </span>
                 </p>
               </div>
             </div>
             <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
               <div className="mb-10 w-full">
-                <h4 className="text-dark mb-4 text-lg font-medium hover:text-[#89ABE3]">
+                <h4 className="text-gray-100 mb-4 text-lg font-medium hover:text-[#89ABE3]">
                   Quick Link
                 </h4>
                 <ul>
                   <li>
                     <a
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                       onClick={() => router.push("/")}
                       style={{ cursor: "pointer" }}
                     >
@@ -693,7 +695,7 @@ const HomeView = (props: Props) => {
                     <a
                       onClick={() => router.push("/#brabout")}
                       style={{ cursor: "pointer" }}
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                     >
                       Who am I ?
                     </a>
@@ -702,7 +704,7 @@ const HomeView = (props: Props) => {
                     <a
                       onClick={() => router.push("/#skills")}
                       style={{ cursor: "pointer" }}
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                     >
                       Skills
                     </a>
@@ -711,7 +713,7 @@ const HomeView = (props: Props) => {
                     <a
                       onClick={() => router.push("/#languages")}
                       style={{ cursor: "pointer" }}
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                     >
                       Languages
                     </a>
@@ -721,7 +723,7 @@ const HomeView = (props: Props) => {
             </div>
             <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
               <div className="mb-10 w-full">
-                <h4 className="text-dark mb-4 text-lg font-medium hover:text-[#89ABE3]">
+                <h4 className="text-gray-100 mb-4 text-lg font-medium hover:text-[#89ABE3]">
                   About
                 </h4>
                 <ul>
@@ -729,7 +731,7 @@ const HomeView = (props: Props) => {
                     <a
                       onClick={() => router.push("/about")}
                       style={{ cursor: "pointer" }}
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                     >
                       Story
                     </a>
@@ -738,7 +740,7 @@ const HomeView = (props: Props) => {
                     <a
                       onClick={() => router.push("/about#timeline")}
                       style={{ cursor: "pointer" }}
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                     >
                       Journey
                     </a>
@@ -747,7 +749,7 @@ const HomeView = (props: Props) => {
                     <a
                       onClick={() => router.push("/about#passions")}
                       style={{ cursor: "pointer" }}
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                     >
                       Passions
                     </a>
@@ -756,7 +758,7 @@ const HomeView = (props: Props) => {
                     <a
                       onClick={() => router.push("/about#contact")}
                       style={{ cursor: "pointer" }}
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                     >
                       Contact
                     </a>
@@ -766,7 +768,7 @@ const HomeView = (props: Props) => {
             </div>
             <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
               <div className="mb-10 w-full">
-                <h4 className="text-dark mb-4 text-lg font-medium hover:text-[#89ABE3]">
+                <h4 className="text-gray-100 mb-4 text-lg font-medium hover:text-[#89ABE3]">
                   Business
                 </h4>
                 <ul>
@@ -774,13 +776,13 @@ const HomeView = (props: Props) => {
                     <a
                       onClick={() => router.push("/work")}
                       style={{ cursor: "pointer" }}
-                      className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                      className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose"
                     >
                       My Projects
                     </a>
                   </li>
                   <li>
-                    <a className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose">
+                    <a className="text-gray-100 hover:text-primary mb-2 inline-block text-base leading-loose">
                       My Resume
                     </a>
                   </li>
@@ -789,14 +791,14 @@ const HomeView = (props: Props) => {
             </div>
             <div className="w-full px-4 sm:w-1/2 lg:w-3/12">
               <div className="mb-10 w-full">
-                <h4 className="text-dark mb-4 text-lg font-medium hover:text-[#89ABE3]">
+                <h4 className="text-gray-100 mb-4 text-lg font-medium hover:text-[#89ABE3]">
                   Contact Me On
                 </h4>
                 <div className="mb-6 flex items-center">
                   <a
                     href="https://github.com/boraozisik"
                     target="_blank"
-                    className="text-dark hover:bg-[#89ABE3] hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+                    className="text-gray-100 hover:bg-[#89ABE3] hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
                   >
                     <svg
                       className="w-5 h-5"
@@ -814,7 +816,7 @@ const HomeView = (props: Props) => {
                   <a
                     href="https://www.linkedin.com/in/boraozisik/"
                     target="_blank"
-                    className="text-dark hover:bg-[#89ABE3] hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+                    className="text-gray-100 hover:bg-[#89ABE3] hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
                   >
                     <svg
                       width="14"
@@ -827,7 +829,7 @@ const HomeView = (props: Props) => {
                   </a>
                   <a
                     href={"mailto:ozisikbora@gmail.com"}
-                    className="text-dark hover:bg-[#89ABE3] hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+                    className="text-gray-100 hover:bg-[#89ABE3] hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
                   >
                     <svg
                       fill="none"
@@ -846,7 +848,7 @@ const HomeView = (props: Props) => {
                   <a
                     href="https://www.instagram.com/borazsk_/"
                     target="_blank"
-                    className="text-dark hover:bg-[#89ABE3] hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
+                    className="text-gray-100 hover:bg-[#89ABE3] hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-white sm:mr-4 lg:mr-3 xl:mr-4"
                   >
                     <svg
                       className="w-5 h-5"
